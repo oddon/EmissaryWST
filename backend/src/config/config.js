318f2f819d0f1701784fa80aa9f1bea5b50f5b1d
Kevin Kuo <kevinstatusk@gmail.com>
2017-06-10 17:49:00 -0700

    switch mLab to my account, properly delete theme after unit test

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -2,7 +2,7 @@
  * Config var for app
 **/
 module.exports = {
-  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://Team1:password@ds155811.mlab.com:55811/team1' || 'mongodb://localhost:27017/webstormtroopers',
+  mongoDBUrl: process.env.MONGODB_URL || process.env.MONGOLAB_URI  || 'mongodb://admin:admin@ds143081.mlab.com:43081/cse112-1-emissary' || 'mongodb://localhost:27017/webstormtroopers',
   port: process.env.PORT || 4941,
   secret: process.env.SECRET || 'mysecret'
 };
